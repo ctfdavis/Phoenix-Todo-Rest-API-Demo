51211f3c148024f942f7e52c04fe170cb71b6946
Davis Chan <davis.chan@aol.com>
2023-09-08 23:20:11 +0800

    fix: pattern matching conn in todo controller

--- lib/phx_todo_api_web/controllers/todo_controller.ex
+++ lib/phx_todo_api_web/controllers/todo_controller.ex
@@ -6,15 +6,18 @@ defmodule PhxTodoApiWeb.TodoController do
 
   action_fallback PhxTodoApiWeb.FallbackController
 
-  def index(conn, _params) do
-    user_id = conn.assigns[:user_id]
+  def index(
+        %Plug.Conn{assigns: %{user_id: user_id}} = conn,
+        _params
+      ) do
     todos = Todos.list_todos_by_user_id(user_id)
     render(conn, :index, todos: todos)
   end
 
-  def create(conn, %{"todo" => todo_params}) do
-    user_id = conn.assigns[:user_id]
-
+  def create(
+        %Plug.Conn{assigns: %{user_id: user_id}} = conn,
+        %{"todo" => todo_params}
+      ) do
     with {:ok, %Todo{} = todo} <- Todos.create_todo(Map.put(todo_params, "user_id", user_id)) do
       conn
       |> put_status(:created)
@@ -23,28 +26,29 @@ defmodule PhxTodoApiWeb.TodoController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    IO.inspect(conn.assigns)
-    user_id = conn.assigns[:user_id]
-    IO.puts("user_id: #{user_id}")
-
+  def show(
+        %Plug.Conn{assigns: %{user_id: user_id}} = conn,
+        %{"id" => id}
+      ) do
     with {:ok, %Todo{} = todo} <- Todos.get_todo_by_id_and_user_id(id, user_id) do
       render(conn, :show, todo: todo)
     end
   end
 
-  def update(conn, %{"id" => id, "todo" => todo_params}) do
-    user_id = conn.assigns[:user_id]
-
+  def update(
+        %Plug.Conn{assigns: %{user_id: user_id}} = conn,
+        %{"id" => id, "todo" => todo_params}
+      ) do
     with {:ok, %Todo{} = todo} <- Todos.get_todo_by_id_and_user_id(id, user_id),
          {:ok, %Todo{} = todo} <- Todos.update_todo(todo, todo_params) do
       render(conn, :show, todo: todo)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    user_id = conn.assigns[:user_id]
-
+  def delete(
+        %Plug.Conn{assigns: %{user_id: user_id}} = conn,
+        %{"id" => id}
+      ) do
     with {:ok, %Todo{} = todo} <- Todos.get_todo_by_id_and_user_id(id, user_id),
          {:ok, %Todo{}} <- Todos.delete_todo(todo) do
       send_resp(conn, :no_content, "")
